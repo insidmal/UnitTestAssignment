e4900a8d1a50b1909941f274a7220a2ca1364d10
khawlaa1 <35938780+khawlaa1@users.noreply.github.com>
2018-03-01 00:28:23 -0800

    getbalance

--- UnitTestAssign/UnitTestProject1/BankAccountTests.cs
+++ UnitTestAssign/UnitTestProject1/BankAccountTests.cs
@@ -1,10 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BankLib;
 
 namespace UnitTestAssign.Tests
 {
+    [TestClass]
     class BankAccountTests
     {
+        [TestMethod]
+        public void TestGetBalance()
+        {
+            BankAccount b = new BankAccount(200);
+            Assert.AreEqual(200, b.getBalance());
+        }
     }
 }
